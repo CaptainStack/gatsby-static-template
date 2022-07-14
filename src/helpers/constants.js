ce97c83c67086b8234fc0a0e6c24a0ff4edebe57
Andre Stackhouse <andre@starlite.Home>
2022-07-13 23:25:40 -0700

    Create site constants file

---
+++ src/helpers/constants.js
@@ -0,0 +1,63 @@
+export const HeaderLinks = [
+  {
+    text: "Home",
+    url: "/",
+  },
+  {
+    text: "About",
+    url: "/about",
+    sublinks: [
+      {
+        text: "People",
+        url: "/about/people",
+      },
+      {
+        text: "Funding",
+        url: "/about/funding",
+      },
+      {
+        text: "Press",
+        url: "/about/press",
+      },
+      {
+        text: "Contact",
+        url: "/about/contact",
+      },
+      {
+        text: "FAQ",
+        url: "/about/faq",
+      },
+    ],
+  },
+  {
+    text: "Blog",
+    url: "/",
+  },
+];
+
+export const AboutSidebarLinks = [
+  {
+    text: "About",
+    url: "/about",
+  },
+  {
+    text: "People",
+    url: "/about/people",
+  },
+  {
+    text: "Funding",
+    url: "/about/funding",
+  },
+  {
+    text: "Press",
+    url: "/about/press",
+  },
+  {
+    text: "Contact",
+    url: "/about/contact",
+  },
+  {
+    text: "FAQ",
+    url: "/about/faq",
+  },
+];
