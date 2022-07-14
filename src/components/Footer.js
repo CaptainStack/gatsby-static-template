a41a0ff14e71e643aa63d4b958f7b63b5c62418a
Andre Stackhouse <andre@starlite.Home>
2022-07-13 23:28:21 -0700

    Create basic site layout components

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+import Logo from "../images/icon.png"
+
+const Footer = () =>
+  <footer>
+    <div className='container'>
+      <div>
+        <a href=''>Twitter</a>
+        <a href=''>Github</a>
+        <a href=''>Email</a>
+      </div>
+      <a href='' target='_blank' rel='noopener noreferrer'><img src={Logo} alt="Logo"/></a>
+      <p>© Organization {new Date().getFullYear()}.</p>
+    </div>
+  </footer>
+
+export default Footer
---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Link } from 'gatsby'
+
+const Sidebar = ({links, pageTitle}) =>
+  <aside>
+      {
+        links.map(link => 
+            <React.Fragment key={link.url}><Link key={link.url} to={link.url} className={pageTitle.localeCompare(link.text, undefined, { sensitivity: 'base' }) === 0 ? 'active' : "inactive"}>{link.text}</Link><hr /></React.Fragment>)
+      }
+  </aside>
+
+export default Sidebar
