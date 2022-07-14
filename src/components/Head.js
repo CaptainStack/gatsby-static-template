ad426fc805e2e41f8ede771bc3f54cc06eaec404
Andre Stackhouse <andre@starlite.Home>
2022-07-14 01:30:23 -0700

    Add site screenshot to metatdata and Readme

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,11 +1,12 @@
 import * as React from "react"
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet'
+import { Screenshot } from '../images/screenshot.png'
 
 const Head = ({pageTitle, siteTitle}) =>
   <Helmet>
     <html lang="en" />
     <title>{pageTitle} | {siteTitle}</title>
-    <meta content='' property="og:image" />
+    <meta content={Screenshot} property="og:image" />
     <meta property="og:description" content="Website meta description" />
   </Helmet>
 
