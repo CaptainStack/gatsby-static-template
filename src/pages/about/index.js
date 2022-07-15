af3254021da4b50c50fd4c51a9fbd6004def2f1a
Andre Stackhouse <andre@starlite.Home>
2022-07-15 10:16:57 -0700

    Add InfoCard to about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Layout from '../../components/Layout'
+import InfoCard from '../../components/InfoCard'
 import { AboutSidebarLinks } from '../../helpers/constants'
 import '../../styles/styles.scss'
 
@@ -9,6 +10,21 @@ const AboutPage = () =>
     <main>
       <title>About Page</title>
       <h1>About heading</h1>
+      <InfoCard
+        title="Info Card 1"
+        description='Description of Info Card 1'
+        link=''
+      />
+      <InfoCard
+        title="Info Card 2"
+        description='Description of Info Card 2'
+        link=''
+      />
+      <InfoCard
+        title="Infocard 3"
+        description='Description of Info Card 3'
+        link=''
+      />
     </main>
     </Layout>
   </div>
