ebf8d7b8180e56bd6343026f2e2eb654b0ec6d7d
Andre Stackhouse <andre@starlite.Home>
2022-07-20 10:48:38 -0700

    Remove redundant tags and attributes (now handled in Layout)

--- src/pages/about/faq.js
+++ src/pages/about/faq.js
@@ -5,16 +5,13 @@ import '../../styles/styles.scss'
 
 const FAQPage = () =>
   <div className='Layout'>
-    <Layout siteTitle='Website' pageTitle="FAQ" sidebarLinks={AboutSidebarLinks} activeHeaderLink="About">
-    <main>
-      <title>FAQ</title>
+    <Layout pageTitle="FAQ" sidebarLinks={AboutSidebarLinks} activeHeaderLink="About">
       <h1>Frequently Asked Questions</h1>
       <ul>
         <li>Question 1</li>
         <li>Question 2</li>
         <li>Question 3</li>
       </ul>
-    </main>
     </Layout>
   </div>
 export default FAQPage
